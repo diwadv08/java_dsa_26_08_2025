1933a7fd1c2a5681b76a5cea6684c6d7ff5f8427
Diwakar M <diwadv813@gmail.com>
2025-09-03 20:28:34 +0530

    New Commit 03-09

--- Basics/New_02_09/Patterns.java
+++ Basics/New_02_09/Patterns.java
@@ -7,7 +7,9 @@ public class Patterns {
         Scanner sc= new Scanner(System.in);
         System.out.print("Enter a pattern rows & columns \t");
         int a=sc.nextInt();
-        System.out.println("The pattern for "+a+"X"+a+ " given below");
+        double rounded_float=Math.round((double)a/2);
+        int center_number=(int) rounded_float;
+        int j,i;
         // System.out.println("----Same Rows & Columns----");
         // for(int i=1;i<=a;i++){
         //     for(int j=1;j<=a;j++){
@@ -15,7 +17,6 @@ public class Patterns {
         //     }
         //     System.out.println();
         // }
-
         // System.out.println("----Like Step----");
         // for(int i=1;i<=a;i++){
         //     for(int j=1;j<=i;j++){
@@ -23,7 +24,6 @@ public class Patterns {
         //     }
         //     System.out.println();
         // }
-
         // System.out.println("----Like Invert Step----");
         // for(int i=1;i<=a;i++){
         //     for(int j=i;j<=a;j++){
@@ -44,7 +44,6 @@ public class Patterns {
         //     }
         //     System.out.println();
         // }
-
         // System.out.println("---------Single direction Cross * ---------------");
         // for(int i=1;i<=a;i++){
         //     for(int j=1;j<=a;j++){
@@ -58,26 +57,82 @@ public class Patterns {
         //     System.out.println();
         // }
 
-        if(a%2==0){
-            System.out.println("Please give only odd numbers");
-        }
-        else{
-            double rounded_float=Math.round((double)a/2);
-            int center_number=(int) rounded_float;
-            System.out.println("----N----");
-            System.out.println(center_number);
-            for(int i=1;i<=a;i++){
-                for(int j=1;j<=a;j++){
-                    if(i==j || j==a-i+1){
-                        System.out.print("*");
-                    }
-                    else{
-                        System.out.print(" ");
-                    }
-                }
-                System.out.println();
+            
+            // System.out.println("----N----");
+            // System.out.println(center_number);
+            // for(int i=1;i<=a;i++){
+            //     for(int j=1;j<=a;j++){
+            //         if(i==j || j==a-i+1){
+            //             System.out.print("*");
+            //         }
+            //         else{
+            //             System.out.print(" ");
+            //         }
+            //     }
+            //     System.out.println();
+            // }
+
+
+        // for(int i=1;i<=a;i++){
+        //     for(int j=1;j<=a;j++){
+        //         if 
+        //             System.out.print("*");
+        //         }
+        //         else{
+        //             System.out.print("|");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        // int num=0;
+        // int myNum;
+        // for(int i=1;i<=a;i++){
+        //     num=(i<=center_number)?i-1:a-i;
+        //     for(int j=1;j<=a;j++){
+        //         myNum=(j<=center_number)?num+=1:num--;
+        //         System.out.print(num);
+        //     }
+        //     System.out.println();
+        // }
+        // sc.close();
+
+        // int num=0;
+        // for(i=1;i<=a;i++){
+        //     num=(i<=center_number)?i:a-i+1;
+        //     for(j=1;j<=a;j++){
+        //         if(i==j || (j==a-i+1)){
+        //             System.out.print(num);
+        //         }
+        //         else{
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        // sc.close();
+
+        // for(i=0;i<a;i++){
+        //     for(j=1;j<=i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(j=1;j<=a;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        int num=1;
+        int a1=1;
+        for(i=1;i<=a;i++){
+            
+            for(j=1;j<=a-i+1;j++){
+                System.out.print(num+++a1);
             }
+            System.out.println();
         }
-        sc.close();
+
+
+
     }
 }
